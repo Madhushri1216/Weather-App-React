2cf3e160767cb0fcbae2a08ba2be748cfbd57092
Madhushri Karekar <madhushrikarekar16@gmail.com>
2025-01-30 21:35:58 +0530

    Add files via upload

---
+++ src/InfoBox.jsx
@@ -0,0 +1,67 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+import "./InfoBox.css";
+
+export default function InfoBox({ info }) {
+  const INIT_URL =
+    "https://plus.unsplash.com/premium_photo-1673081112888-f877c594ad18?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aGF6ZSUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+
+  let COLD_URL =
+    "https://plus.unsplash.com/premium_photo-1674808697474-499c766ed8b4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTA1fHxjb2xkfGVufDB8fDB8fHww";
+  let HOT_URL =
+    "https://images.unsplash.com/photo-1524594081293-190a2fe0baae?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG90JTIwd2VhdGhlciUyMHRyZWV8ZW58MHx8MHx8fDA%3D";
+  let RAIN_URL =
+    "https://images.unsplash.com/photo-1519692933481-e162a57d6721?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cmFpbnxlbnwwfHwwfHx8MA%3D%3D";
+  return (
+    <div className="InfoBox">
+      <div className="cardContainer">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                ? HOT_URL
+                : COLD_URL
+            }
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {info.city} &nbsp;
+              {info.humidity > 80 ? (
+                <ThunderstormIcon />
+              ) : info.temp > 15 ? (
+                <WbSunnyIcon />
+              ) : (
+                <AcUnitIcon />
+              )}
+            </Typography>
+            <Typography
+              variant="body2"
+              color="text.secondary"
+              component={"span"}
+            >
+              <div>
+                <p>Temperatur = {info.temp}&deg;C</p>
+                <p>Humidity = {info.humidity}</p>
+                <p>Min Temp = {info.tempMin}&deg;C</p>
+                <p>Max Temp = {info.tempMax}&deg;C</p>
+                <p>
+                  The weather can be described as <i>{info.weather}</i> and
+                  feels like {info.feelslike}&deg;C
+                </p>
+              </div>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
